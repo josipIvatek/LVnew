ad98574e13697a017d6e4e11ac3c8ecc0142c59e
josipIvatek <jivatek@tvz.hr>
2023-03-21 15:50:46 +0100

    LV2

---
+++ lv1 main.py
@@ -0,0 +1,12 @@
+
+# Ova linija mora biti komentar!
+print('Dobrodošli', '!'*3)
+# Napisati sljedeće izraze u jednoj liniji koda
+ime = input('Unesite vaše ime: ')
+prezime = input('Unesite vaše prezime: ')
+JMBAG = input('Unesite vaš JMBAG: ')
+# TODO zatražite od korisnika broj godina
+# TODO ime i prezime ispišite u navodnim znakovima
+godine = input('Unesite broj svojih godina: ')
+print("Korisnik : ", ime, " ", prezime, "", godine, "", end='!')
+
